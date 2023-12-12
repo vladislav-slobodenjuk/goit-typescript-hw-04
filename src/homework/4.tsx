e2cb691dfff381571cae75f3734fffcb6549e2ff
vladislav-slobodenjuk <vladjun@gmail.com>
2023-12-13 00:31:18 +0200

    task04

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,23 +1,39 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
+import {
+  createContext,
+  useMemo,
+  useState,
+  useContext,
+  ReactElement,
+} from "react";
 import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
 
+type SelectedMenu = {
+  id?: MenuIds;
+};
+
 // Додати тип Menu Selected
+type MenuSelected = {
+  selectedMenu: SelectedMenu;
+};
 
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
 
 // Додайте тип MenuAction
+type MenuAction = {
+  onSelectedMenu: (selected: SelectedMenu) => void;
+};
 
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+  children: ReactElement; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
@@ -48,7 +64,7 @@ function MenuProvider({ children }: PropsProvider) {
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+  menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
